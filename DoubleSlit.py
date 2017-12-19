4b8245004871c1c1bfaa01a917640d6346af6cf1
maximbeekenkamp <34655192+maximbeekenkamp@users.noreply.github.com>
2017-12-19 16:48:14 +0100

    List assignment index out of range

--- DoubleSlit.py
+++ DoubleSlit.py
@@ -13,15 +13,18 @@ d = 2.8*10**-6 #input("Enter distance between slits: ")
 D = 0.8 #input("Enter distance between slits and the screen: ")
 I_initial = input("Enter initial intensity of light: ") 
 Theta = 0.01 #math.atan(x/D) 
-F = [[0 for m in range(0,a+1) ] for n in range(0,b)]
-I = [[0 for m in range(0,a) ] for n in range(0,b)]
+#F = [[0 for m in range(0,a+1) ] for n in range(0,b)]
+I = []#[[0 for m in range(0,a) ] for n in range(0,b)]
 
-for y in range (0, b):
-	I[y] = I_initial*math.cos(((math.pi*d*math.sin(Theta))/(wl))*(((math.sin(math.pi*W*math.sin(Theta/wl))/(math.pi*W*math.sin(Theta/wl))))**2))**2
-	print (I[y])
-	F = [I[y], Theta]
+X = []
+Y = []
+
+for x in range (0, b):
+	I[x] = I_initial*math.cos(((math.pi*d*math.sin(Theta))/(wl))*(((math.sin(math.pi*W*math.sin(Theta/wl))/(math.pi*W*math.sin(Theta/wl))))**2))**2
+	print (I[x])
+	X = I[x]
+	Y = Theta
 	Theta = Theta + 0.01
 
-x,y = F
-plt.scatter(x,y)
+plt.plot(X,Y)
 plt.show()
